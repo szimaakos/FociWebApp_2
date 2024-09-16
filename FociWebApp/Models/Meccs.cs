395d0a0ee57456d8f3dedabcf81a0c310dbf08df
Sutak0 <sutak.david.5@gmail.com>
2024-09-16 08:39:11 +0200

    Add project files.

---
+++ FociWebApp/Models/Meccs.cs
@@ -0,0 +1,11 @@
+﻿namespace FociWebApp.Models
+{
+    public class Meccs
+    {
+        public int Id { get; set; }
+        public int Fordulo { get; set; }
+        public int HazaiVeg { get; set; }
+        public int VendegVeg { get; set; }
+        public int HazaiFelido  { get; set; }
+    }
+}
